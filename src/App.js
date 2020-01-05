a65926b14ea1721358f87387a7fe6ed2d78f6c54
Sean <spward@coastal.edu>
2020-01-04 19:26:08 -0500

    Cleaned up files & file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.scss";
-import Header from "./Header";
-import Home from "../routes/Home";
-import About from "../routes/About";
-import Portfolio from "../routes/Portfolio";
-import Experience from "../routes/Experience";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Portfolio from "./routes/Portfolio";
+import Experience from "./routes/Experience";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import "./assets/css/flexboxgrid.css";
-import App from "./components/App";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
