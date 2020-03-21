435ad4f3272886a421380ca1b9fe29ce91836859
spward <spward@coastal.edu>
2020-03-20 22:10:22 -0400

    Fixed Portfolio layout

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -6,37 +6,22 @@ import projects from "../../../assets/projects.json";
 const Portfolio = () => {
   return (
     <div className="portfolio container container-fluid">
-      <h3>My Portfolio</h3>
-      <div className="portfolio__filter">
-        <ul className="filter list">
-          <li className="active" data-filter="*">
-            <button>All Categories</button>
-          </li>
-          <li data-filter=".brand">
-            <button href="#">Branding</button>
-          </li>
-          <li data-filter=".webdev">
-            <button>Web Development</button>
-          </li>
-          <li data-filter=".web">
-            <button>Web Design</button>
-          </li>
-        </ul>
-
-        <div className="projects">
-          {projects.map(project => (
-            <Link to={`portfolio/${project.name}`} key={project.id}>
-              <div
-                className={
-                  "project--item card " + project.tech.join(" ").toLowerCase()
-                }
-                key={project.id}
-              >
-                <h3>{project.name}</h3>
+      <div className="projects">
+        {projects.map(project => (
+          <Link to={`portfolio/${project.name}`} key={project.id}>
+            <div className={"card"} key={project.id}>
+              <div className="project__item">
+                <img
+                  src={`/projects/thumbnails/${project.thumbnail}`}
+                  alt={project.name}
+                />
+                <div className="project__item--text">
+                  <h3>{project.name}</h3>
+                </div>
               </div>
-            </Link>
-          ))}
-        </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
